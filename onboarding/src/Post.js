046594835571e6bb8f0c376e9ff5c4815fb761b8
Genevieve <genevievechin@g.ucla.edu>
2021-12-03 15:30:16 -0800

    start guide

---
+++ onboarding/src/Post.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default function Post({ author, body }) {
+  return (
+		<>
+      <div>
+        Author:
+        {author}
+      </div>
+      <div>
+        Body:
+        {body}
+      </div>
+    </>
+  );
+}
+
+Post.propTypes = {
+  author: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+};
